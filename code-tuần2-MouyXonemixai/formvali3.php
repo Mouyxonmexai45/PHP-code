02ed17f689f05ea1d07db59b4c6b817579c7ca5a
Mouyxonmexai45 <167184702+Mouyxonmexai45@users.noreply.github.com>
2024-04-16 21:24:38 +0700

    Add files via upload

---
+++ code-tuần2-MouyXonemixai/formvali3.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+
+    <body>
+    <?php 
+    echo "Ch2> Your Input: </h2>";
+     echo $name; 
+     echo "<br>"; 
+    echo $email;
+     echo "<br>"; 
+    echo $website; 
+    echo "<br>";
+     echo $comment; 
+     echo "<br>"; 
+    echo $gender;
+    ?>
+    </body>
+</html>
---
+++ code-tuần2-MouyXonemixai/vduxulyfrom.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+
+    <body>
+    <!doctype html>
+<html lang="en">
+    <body>
+        <header>
+        </header>
+        <main>
+        <form action="welcome.php" method="post">
+        <div class="mb-3">
+            <label for="" class="form-label">Name</label>
+            <input
+                type="text"
+                class="form-control"
+                name="name"
+                id=""
+                aria-describedby="helpId"
+                placeholder=""
+            />
+            <small id="helpId" class="form-text text-muted">Help text</small>
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Email</label>
+            <input
+                type="email"
+                class="form-control"
+                name="email"
+                id=""
+                aria-describedby="emailHelpId"
+                placeholder="abc@mail.com"
+            />
+            <small id="emailHelpId" class="form-text text-muted"
+                >Help text</small
+            >
+        </div>
+        <button
+            type="submit"
+            class="btn btn-primary"
+        >
+            Submit
+        </button>
+        </form>
+        </main>
+    </body>
+</html>
